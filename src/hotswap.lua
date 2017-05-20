4ff4fa83e18574af3649d376e1e9fee043feb984
Cameron McHenry <camchenry@users.noreply.github.com>
2017-05-20 19:14:39 -0400

    Update all file info on file change

--- src/hotswap.lua
+++ src/hotswap.lua
@@ -43,6 +43,21 @@ function hotswap:hook(filepath, fn)
     end
 end
 
+function hotswap:update_file_info(filepath)
+    if love.filesystem.exists(filepath) then
+        self.last_modified[filepath] = love.filesystem.getLastModified(filepath)
+        self.hashes[filepath] = xxhash(love.filesystem.read(filepath))
+    end
+end
+
+function hotswap:on_file_changed(filepath)
+    print('File changed: ' .. filepath)
+    for i, fn in ipairs(self.hooks[filepath]) do
+        fn(filepath)
+    end
+    self:update_file_info(filepath)
+end
+
 function hotswap:check_file_modified()
     -- Scan for file changes
     for filepath, hooks in pairs(self.hooks) do
@@ -53,14 +68,10 @@ function hotswap:check_file_modified()
 
             if last_modified and last_modified > self.last_modified[filepath] then
                 changed = true
-                self.last_modified[filepath] = last_modified
             end
 
             if changed then
-                print('File changed: ' .. filepath)
-                for i, fn in ipairs(hooks) do
-                    fn(filepath)
-                end
+                self:on_file_changed(filepath)
             end
         end
     end
@@ -76,14 +87,10 @@ function hotswap:check_file_hashes()
 
             if new_hash ~= self.hashes[filepath] then
                 changed = true
-                self.hashes[filepath] = new_hash
             end
 
             if changed then
-                print('File changed: ' .. filepath)
-                for i, fn in ipairs(hooks) do
-                    fn(filepath)
-                end
+                self:on_file_changed(filepath)
             end
         end
     end
