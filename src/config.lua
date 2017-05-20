42d55482b7caf3081ac2ba24a03a79a8de0f28e4
Cameron McHenry <camchenry@users.noreply.github.com>
2017-05-20 16:14:21 -0400

    Keep arrays in tables in the config data

--- src/config.lua
+++ src/config.lua
@@ -5,7 +5,11 @@ local config = {
 function config:mixin(t)
     for k, v in pairs(t) do
         if type(v) == "table" then
-            self:mixin(v)
+            if Lume.isarray(v) then
+                self.data[k] = v
+            else
+                self:mixin(v)
+            end
         else
             self.data[k] = v
         end
