0b046c7e44367e1f299445ee5c580df8e0508ae6
Cameron McHenry <camchenry@users.noreply.github.com>
2017-11-13 22:55:01 -0500

    Fix order of debug stats

--- src/gui.lua
+++ src/gui.lua
@@ -86,6 +86,14 @@ local gui = {}
 gui.Instance = Class{}
 
 function gui.Instance:init(props)
+    self.x = 5
+    self.y = 5
+    self.padding_x = 10
+    self.padding_y = 5
+    self.width = love.graphics.getWidth()
+    self.height = love.graphics.getHeight() * 0.3
+    self.row_width = self.width
+    self.row_height = 15
 
     if props then
         for k, v in pairs(props) do
@@ -99,48 +107,48 @@ function gui.Instance:update(dt)
 end
 
 function gui.Instance:draw()
-    local x = 10
-    local y = 10
-    local padding_x = 5
-    local padding_y = 5
-    local row_width = 200
-    local row_height = 20
+    local x = self.x
+    local y = self.y
+    local padding_x = self.padding_x
+    local padding_y = self.padding_y
+    local row_width = self.row_width
+    local row_height = self.row_height
     local w = row_width
     local h = (row_height + padding_y) * 9
 
-    love.graphics.setColor(0, 0, 0, 64)
-    love.graphics.rectangle('fill', x, y, w, h)
+    love.graphics.setColor(0, 0, 0, 128)
+    love.graphics.rectangle('fill', x - padding_x, y - padding_y, w + padding_x * 2, h + padding_y * 2)
     suit:draw()
 end
 
+local info = {}
 function gui.Instance:draw_performance()
-    local x = 10
-    local y = 10
-    local padding_x = 5
-    local padding_y = 5
-    local row_width = 200
-    local row_height = 20
+    local x = self.x
+    local y = self.y
+    local padding_x = self.padding_x
+    local padding_y = self.padding_y
+    local row_width = self.row_width
+    local row_height = self.row_height
 
     local stats = love.graphics.getStats()
     local unit = "MB"
     local ram = collectgarbage("count") / 1024
     local vram = stats.texturememory / 1024 / 1024
-    local info = {
-        fps             = "FPS: " .. ("%3d"):format(love.timer.getFPS()),
-        delta           = ("Delta: %.3fms"):format(love.timer.getAverageDelta() * 1000),
-        ram             = ("RAM: %.2f%s"):format(ram, unit),
-        vram            = ("VRAM: %.2f%s"):format(vram, unit),
-        draw_calls      = "Draws: " .. stats.drawcalls,
-        canvases        = "Canvases: " .. stats.canvases,
-        canvas_switches = "Switches: " .. stats.canvasswitches,
-        images          = "Images: " .. stats.images,
-        shader_switches = "Shader switches: " .. stats.shaderswitches,
-    }
 
     suit.layout:reset(x, y, padding_x, padding_y)
+    love.graphics.setFont(Fonts.monospace[15])
 
-    for i, v in pairs(info) do
-        love.graphics.setFont(Fonts.monospace[15])
+    info[1] = "FPS: " .. ("%3d"):format(love.timer.getFPS())
+    info[2] = ("Delta: %.3fms"):format(love.timer.getAverageDelta() * 1000)
+    info[3] = ("RAM: %.2f%s"):format(ram, unit)
+    info[4] = ("VRAM: %.2f%s"):format(vram, unit)
+    info[5] = "Draws: " .. stats.drawcalls
+    info[6] = "Canvases: " .. stats.canvases
+    info[7] = "Switches: " .. stats.canvasswitches
+    info[8] = "Images: " .. stats.images
+    info[9] = "Shader switches: " .. stats.shaderswitches
+
+    for i, v in ipairs(info) do
         suit:Label(v, {align = 'left'}, suit.layout:row(row_width, row_height))
     end
 end
